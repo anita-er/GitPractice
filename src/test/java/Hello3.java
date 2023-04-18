ddcde485097858352d9210796e0a131ead4b5bdb
AneethaEntoor <aneetha.entoor@skills-base.com>
2023-04-18 13:22:11 +1000

    hello4 added

--- src/test/java/Hello3.java
+++ src/test/java/Hello3.java
@@ -1,5 +1,8 @@
 public class Hello3 {
     public static void main(String[] args) {
         System.out.print("Hello");
+
+        System.out.print("Hello");
+        System.out.print("Hello");
     }
 }
